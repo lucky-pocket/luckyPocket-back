21da615438946658650451e808316aa46e1b8034
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-10-23 15:38:05 +0900

    add :: user useCase 작성

---
+++ internal/app/user/usecase/user_usecase.go
@@ -0,0 +1,85 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/constant"
+
+	"github.com/lucky-pocket/luckyPocket-back/internal/global/auth"
+
+	"github.com/lucky-pocket/luckyPocket-back/internal/app/user/util"
+
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain"
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/input"
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/output"
+)
+
+type Deps struct {
+	UserRepository   domain.UserRepository
+	PocketRepository domain.PocketRepository
+}
+
+type userUseCase struct{ *Deps }
+
+func NewUserUseCase(deps *Deps) domain.UserUseCase {
+	return &userUseCase{deps}
+}
+
+func (uc *userUseCase) GetMyDetail(ctx context.Context) (*output.MyDetailOutput, error) {
+	userInfo := auth.MustExtract(ctx)
+
+	user, err := uc.UserRepository.FindByID(ctx, userInfo.UserID)
+
+	util.CheckUserNil(user, err)
+
+	//TODO : Notice Service 작성시 hasNewNotification 에 대한 로직 추가
+	return util.ToUserToOutput(*user, true), nil
+}
+
+func (uc *userUseCase) CountCoins(ctx context.Context) (*output.CoinOutput, error) {
+
+	userInfo := auth.MustExtract(ctx)
+
+	user, err := uc.UserRepository.FindByID(ctx, userInfo.UserID)
+
+	util.CheckUserNil(user, err)
+
+	return &output.CoinOutput{
+		Coins: user.Coins,
+	}, nil
+}
+
+func (uc *userUseCase) GetUserDetail(ctx context.Context) (*output.UserInfo, error) {
+	userInfo := auth.MustExtract(ctx)
+
+	user, err := uc.UserRepository.FindByID(ctx, userInfo.UserID)
+
+	util.CheckUserNil(user, err)
+
+	return util.ToUserInfo(*user), nil
+}
+
+func (uc *userUseCase) GetRanking(ctx context.Context, input *input.UserInput) (*output.RankOutput, error) {
+
+	constant.SortType.Valid(input.SortType)
+
+	var (
+		users []output.RankElem
+		err   error
+	)
+
+	switch input.UserType {
+	case constant.TypeStudent:
+		users, err = uc.UserRepository.FindStudentsByFilter(ctx, input.SortType, input.Name, input.Class, input.Grade)
+	default:
+		users, err = uc.UserRepository.FindNonStudentByFilter(ctx, input.SortType, input.Name)
+	}
+
+	if err != nil {
+		return nil, errors.Wrap(err, "UnExpected DB Error")
+	}
+
+	return util.RankOutput(users), nil
+}
