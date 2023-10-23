3e2eb3f61302605adf40796246d0b4c3aa4f92e7
김원욱 <kimww0306@gmail.com>
2023-10-23 08:15:42 +0000

    test :: issuer & parser test 작성

---
+++ test/unit/global/auth/jwt/issue_test.go
@@ -0,0 +1,45 @@
+package jwt_test
+
+import (
+	"testing"
+
+	jwtgo "github.com/golang-jwt/jwt/v5"
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/constant"
+	"github.com/lucky-pocket/luckyPocket-back/internal/global/auth"
+	"github.com/lucky-pocket/luckyPocket-back/internal/global/auth/jwt"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIssue(t *testing.T) {
+	secret := []byte("hi new secret")
+	info := auth.Info{
+		UserID: 1,
+		Role:   constant.RoleAdmin,
+	}
+
+	issuer := jwt.NewIssuer(secret)
+
+	validate := func(token string) error {
+		_, err := jwtgo.ParseWithClaims(token, &jwt.Token{}, func(t *jwtgo.Token) (interface{}, error) {
+			return secret, nil
+		})
+
+		return err
+	}
+
+	t.Run("access", func(t *testing.T) {
+		accessToken := issuer.IssueAccess(info)
+
+		err := validate(accessToken)
+
+		assert.NoError(t, err)
+	})
+
+	t.Run("refresh", func(t *testing.T) {
+		refreshToken := issuer.IssueRefresh(info)
+
+		err := validate(refreshToken)
+
+		assert.NoError(t, err)
+	})
+}
