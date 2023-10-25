f6471abb20d574f9b0d59390d93373bdf093e309
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-10-25 20:14:04 +0900

    fix :: domain field 수정

--- internal/domain/notice.go
+++ internal/domain/notice.go
@@ -2,17 +2,19 @@ package domain
 
 import (
 	"context"
+	"time"
 
 	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/constant"
 	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/output"
 )
 
 type Notice struct {
-	NoticeID uint64
-	User     *User
-	Pocket   *Pocket
-	Type     constant.NoticeType
-	Checked  bool
+	NoticeID  uint64
+	User      *User
+	Pocket    *Pocket
+	Type      constant.NoticeType
+	Checked   bool
+	CreatedAt time.Time
 }
 
 type NoticeUseCase interface {
@@ -21,6 +23,6 @@ type NoticeUseCase interface {
 
 type NoticeRepository interface {
 	Create(ctx context.Context, notices []*Notice) error
-	FindAllByUserID(ctx context.Context, userID uint64) (*output.NoticeListOutput, error)
+	FindAllByUserID(ctx context.Context, userID uint64) (*[]*Notice, error)
 	FindByID(ctx context.Context, noticeID uint64) (*Notice, error)
 }
