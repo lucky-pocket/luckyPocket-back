c7e7e6722ec1564a82089b467922a9b50e16c18e
onee-only <kimww0306@gmail.com>
2023-10-26 15:27:12 +0900

    add :: Transactor stub 추가

---
+++ test/stubs/stubs.go
@@ -0,0 +1,43 @@
+package stubs
+
+import (
+	"testing"
+
+	"github.com/lucky-pocket/luckyPocket-back/test/mocks"
+)
+
+type PocketRepository struct {
+	*mocks.PocketRepository
+	*stubTransactor
+}
+
+func NewPocketRepository(t *testing.T) *PocketRepository {
+	return &PocketRepository{
+		stubTransactor:   &stubTransactor{},
+		PocketRepository: mocks.NewPocketRepository(t),
+	}
+}
+
+type UserRepository struct {
+	*mocks.UserRepository
+	*stubTransactor
+}
+
+func NewUserRepository(t *testing.T) *UserRepository {
+	return &UserRepository{
+		stubTransactor: &stubTransactor{},
+		UserRepository: mocks.NewUserRepository(t),
+	}
+}
+
+type NoticeRepository struct {
+	*mocks.NoticeRepository
+	*stubTransactor
+}
+
+func NewNoticeRepository(t *testing.T) *NoticeRepository {
+	return &NoticeRepository{
+		stubTransactor:   &stubTransactor{},
+		NoticeRepository: mocks.NewNoticeRepository(t),
+	}
+}
