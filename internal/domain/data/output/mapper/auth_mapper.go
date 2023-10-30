b4b6cb9b8ea24300be85139d5936c0fc434671b2
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-10-30 20:24:52 +0900

    fix :: convention 적용

--- internal/domain/data/output/mapper/auth_mapper.go
+++ internal/domain/data/output/mapper/auth_mapper.go
@@ -6,15 +6,15 @@ import (
 	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/output"
 )
 
-func ToTokenOutput(access, refresh string, accessExpiredAt, refreshExpiredAT time.Time) *output.TokenOutput {
+func ToTokenOutput(access, refresh string, accessExp, refreshExp time.Time) *output.TokenOutput {
 	return &output.TokenOutput{
 		Access: output.TokenElem{
 			Token:     access,
-			ExpiresAt: accessExpiredAt,
+			ExpiresAt: accessExp,
 		},
 		Refresh: output.TokenElem{
 			Token:     refresh,
-			ExpiresAt: refreshExpiredAT,
+			ExpiresAt: refreshExp,
 		},
 	}
 }
