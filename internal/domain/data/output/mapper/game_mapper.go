f4ea5e65c61934a56047aa067ce146753d5b8d86
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-10-31 20:13:10 +0900

    add :: game_mapper

---
+++ internal/domain/data/output/mapper/game_mapper.go
@@ -0,0 +1,24 @@
+package mapper
+
+import (
+	"time"
+
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/output"
+)
+
+func ToYutOutput(marked bool, yutPieces [3]bool, coinsEarned int) *output.YutOutput {
+	return &output.YutOutput{
+		Result: output.YutResultElem{
+			Marked:    marked,
+			YutPieces: yutPieces,
+		},
+		CoinsEarned: coinsEarned,
+	}
+}
+
+func ToFreeTicketOutput(count int, reFillAt time.Time) *output.TicketOutput {
+	return &output.TicketOutput{
+		RefillAt:    reFillAt,
+		TicketCount: count,
+	}
+}
