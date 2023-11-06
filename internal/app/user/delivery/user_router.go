3e90a0c9c87e94f3ca314f3434ca3bc1bc7e2d1c
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-07 08:30:56 +0900

    add :: router 설정

---
+++ internal/app/user/delivery/user_router.go
@@ -0,0 +1,116 @@
+package delivery
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain"
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/constant"
+	"github.com/lucky-pocket/luckyPocket-back/internal/domain/data/input"
+)
+
+type UserRouter struct {
+	userUseCase domain.UserUseCase
+}
+
+func NewUserRouter(uc domain.UserUseCase) *UserRouter {
+	return &UserRouter{uc}
+}
+
+func (r *UserRouter) Register(engine *gin.Engine) {
+	engine.GET("/users/:userID", r.getUserDetail)
+	engine.GET("/users/me/coins", r.countCoins)
+	engine.GET("/users/me", r.getMyDetail)
+	engine.GET("/rank", r.getRanking)
+	engine.GET("/search", r.search)
+}
+
+func (r *UserRouter) getUserDetail(c *gin.Context) {
+	id := c.Param("userID")
+
+	userID, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	userInfo, err := r.userUseCase.GetUserDetail(c, &input.UserIDInput{UserID: userID})
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, userInfo)
+}
+
+func (r *UserRouter) countCoins(c *gin.Context) {
+	coins, err := r.userUseCase.CountCoins(c)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, coins)
+}
+
+func (r *UserRouter) getMyDetail(c *gin.Context) {
+	res, err := r.userUseCase.GetMyDetail(c)
+	if err != nil {
+		c.Error(err)
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func (r *UserRouter) getRanking(c *gin.Context) {
+	var rank Rank
+
+	if err := c.ShouldBindQuery(&rank); err != nil {
+		c.Error(err)
+		return
+	}
+
+	res, err := r.userUseCase.GetRanking(c, &input.RankQueryInput{
+		SortType: rank.SortType,
+		UserType: rank.UserType,
+		Grade:    rank.grade,
+		Class:    rank.class,
+		Name:     rank.name,
+	})
+	if err != nil {
+		c.Error(err)
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+func (r *UserRouter) search(c *gin.Context) {
+	var query Query
+
+	if err := c.ShouldBindQuery(&query); err != nil {
+		c.Error(err)
+		return
+	}
+
+	res, err := r.userUseCase.Search(c, &input.SearchInput{
+		Query: query.Query,
+	})
+	if err != nil {
+		c.Error(err)
+	}
+
+	c.JSON(http.StatusOK, res)
+}
+
+type Rank struct {
+	SortType constant.SortType  `form:"sortType" binding:"required,enum"`
+	UserType *constant.UserType `form:"userType" binding:"enum"`
+	grade    *int               `form:"grade" binding:"number"`
+	class    *int               `form:"class" binding:"number"`
+	name     *string            `form:"name"`
+}
+
+type Query struct {
+	Query string
+}
