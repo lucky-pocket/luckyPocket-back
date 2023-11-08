4cfd2f45f6ba93c9a4b11bff81be5c38b97988ef
onee-only <kimww0306@gmail.com>
2023-11-08 14:13:04 +0900

    update :: batch test 수정

--- test/unit/global/batch/notice_sender_test.go
+++ test/unit/global/batch/notice_sender_test.go
@@ -1,12 +1,15 @@
 package batch_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/lucky-pocket/luckyPocket-back/internal/domain"
 	"github.com/lucky-pocket/luckyPocket-back/internal/global/batch"
 	"github.com/lucky-pocket/luckyPocket-back/test/mocks"
 	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func TestNoticeSender(t *testing.T) {
@@ -16,12 +19,16 @@ func TestNoticeSender(t *testing.T) {
 	n := batch.NewNoticeSender(&batch.NoticeSenderDeps{
 		NoticeRepository: mockNoticeRepository,
 		NoticePool:       mockNoticePool,
+		Logger: zap.New(zapcore.NewCore(
+			zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()),
+			os.Stdout,
+			zap.DebugLevel,
+		)),
 	})
 
-	mockNoticePool.On("Take", mock.Anything, mock.Anything).Return([]*domain.Notice{}, nil)
+	mockNoticePool.On("Take", mock.Anything, mock.Anything).Return([]*domain.Notice{{}}, nil)
 	mockNoticeRepository.On("CreateBulk", mock.Anything, mock.Anything).Return(nil)
 
-	// TODO: add test for log when it is added.
 	n.Do()
 
 	mockNoticePool.AssertExpectations(t)
