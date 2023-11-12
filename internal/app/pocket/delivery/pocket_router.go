4cc373abc369a4a7f756ef8430f0f94e42d8c22a
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-11-12 17:11:57 +0900

    fix :: coin min binding 제거

--- internal/app/pocket/delivery/pocket_router.go
+++ internal/app/pocket/delivery/pocket_router.go
@@ -18,7 +18,7 @@ type PocketQuery struct {
 
 type PocketRequest struct {
 	ReceiverID uint64 `json:"receiverID" binding:"required"`
-	Coins      int    `json:"coins" binding:"required,number,min=2"`
+	Coins      int    `json:"coins" binding:"required,number"`
 	Message    string `json:"message" binding:"required,max=300"`
 	IsPublic   bool   `json:"isPublic" binding:"required"`
 }
